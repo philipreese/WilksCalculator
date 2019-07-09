f87bdf4beb364a2a4b5aa220b3a221b50a98536a
Philip Reese <philipreese@mail.com>
2019-07-09 14:06:31 -0400

    #2-implement-calculator Add handling for changing units Implement text change and calculation conversion

--- script.js
+++ script.js
@@ -11,9 +11,35 @@ function calculateWilks() {
     let bodyweight = document.wilksForm.bodyweight.value;
     let total = document.wilksForm.total.value;
 
+    if (document.wilksForm.units.value === "lbs") {
+        let weights = [bodyweight, total];
+        weights.forEach((kgWeight, index, arr) => {
+            arr[index] = kgWeight / kgsToPounds;
+        });
+
+        bodyweight = weights[0];
+        total = weights[1];
+    }
+
     let wilksScore = total * (numerator / (constant + (coeff1 * bodyweight) + (coeff2 * (bodyweight**2)) + (coeff3 *(bodyweight**3)) + (coeff4 * (bodyweight**4)) + (coeff5 * (bodyweight**5)))).toFixed(2);
     wilksScore = +wilksScore.toFixed(2);
 
     document.getElementById("wilksOutput").textContent = "Wilks Score: " + wilksScore;
 }
+
+document.wilksForm.units.addEventListener("change", function() {
+    let legend = document.getElementById("legend");
+    let bodyweight = document.getElementById("bodyweight");
+    let total = document.getElementById("total");
+
+    if(document.wilksForm.units.value === "lbs") {
+        legend.textContent = "Calculate Wilks Points in pounds (lbs)";
+        bodyweight.childNodes[0].textContent = "Bodyweight (lbs): ";
+        total.childNodes[0].textContent = "Lifted Weight (lbs): ";
+    } else {
+        legend.textContent = "Calculate Wilks Points in kilograms (kgs)";
+        bodyweight.childNodes[0].textContent = "Bodyweight (kgs): ";
+        total.childNodes[0].textContent = "Lifted Weight (kgs): ";
+    }
+});
 
